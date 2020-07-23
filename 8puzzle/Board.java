aa9a1faa050428e46d952a1f91ef5d74d25e7956
AndrewN <andrew.nwuba@gmail.com>
2020-07-22 23:14:51 -0400

    iterable messed up and priority queue missing checks

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -4,6 +4,8 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Queue;
+
 public class Board {
 
     // create a board from an n-by-n array of tiles,
@@ -16,7 +18,15 @@ public class Board {
 
     // string representation of this board
     public String toString() {
-
+        StringBuilder s = new StringBuilder();
+        s.append(this.board.length + "\n");
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board.length; j++) {
+                s.append(String.format("%2d ", board[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 
     // board dimension n
@@ -29,22 +39,22 @@ public class Board {
         int misplacedPieces = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (i == board.length -1 && j == board.length - 1) {
-                    return misplacedPieces;
-                }
-                if (this.board[i][j] !=  ((i+1) + (j * this.board.length))) {
-                    misplacedPieces++;
+                if (i != board.length - 1 && j != board.length - 1) {
+                    if (this.board[i][j] != ((i + 1) + (j * this.board.length))) {
+                        misplacedPieces++;
+                    }
                 }
             }
         }
+        return misplacedPieces;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
         int manhattanDistance = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if(this.board[i][j] != 0) {
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board.length; j++) {
+                if (this.board[i][j] != 0) {
                     manhattanDistance += this.properRowDistance(this.board[i][j], j);
                     manhattanDistance += this.properColDistance(this.board[i][j], i);
                 }
@@ -76,24 +86,39 @@ public class Board {
 
     // is this board the goal board?
     public boolean isGoal() {
-        if (this.hamming() == 0) {
-            return true;
-        } else {
-            return false;
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board.length; j++) {
+                if (i != this.board.length -1 && j != this.board.length -1) {
+                    if (this.board[i][j] != ((i+1) + (j * this.board.length))) {
+                        return false;
+                    }
+                } else {
+                    if (this.board[i][j] != 0) {
+                        return false;
+                    }
+                }
+            }
         }
+        return true;
     }
 
     // does this board equal y?
     public boolean equals(Object y) {
-        if (!(y instanceof int[][])) {
+        if (y == this) {
+            return true;
+        }
+        if (y == null) {
             return false;
         }
-        if (this.board.length != y.length) {
+        if (y.getClass() != this.getClass()) {
+            return false;
+        }
+        if (board.length != ((Board) y).board.length) {
            return false;
         }
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (this.board[i][j] != y[i][j]) {
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board.length; j++) {
+                if (this.board[i][j] != ((Board) y).board[i][j]) {
                    return false;
                 }
             }
@@ -103,14 +128,79 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        //create a list of all numbers touching the 0 or blank space
-        //create a list of of all boards where you switch the 0 with a number touching the blank space
+        int zeroRow = 0;
+        int zeroCol = 0;
+        Board neighbor;
+        Queue<Board> q = new Queue<>();
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board.length; j++) {
+                if (this.board[i][j] == 0) {
+                    zeroRow = i;
+                    zeroCol = j;
+                    break;
+                }
+            }
+        }
+        if (zeroRow == 0) {
+            neighbor = new Board(board);
+            swap(neighbor, zeroRow, zeroCol, zeroRow +1, zeroCol);
+            q.enqueue(neighbor);
+        } else if (zeroRow == this.board.length -1) {
+            neighbor = new Board(board);
+            swap(neighbor, zeroRow, zeroCol, zeroRow -1, zeroCol);
+            q.enqueue(neighbor);
+        } else {
+            neighbor = new Board(board);
+            swap(neighbor, zeroRow, zeroCol, zeroRow +1, zeroCol);
+            q.enqueue(neighbor);
+            neighbor = new Board(board);
+            swap(neighbor, zeroRow, zeroCol, zeroRow -1, zeroCol);
+            q.enqueue(neighbor);
+        }
 
+        if (zeroCol == 0) {
+            neighbor = new Board(board);
+            swap(neighbor, zeroRow, zeroCol, zeroRow, zeroCol +1);
+            q.enqueue(neighbor);
+        } else if (zeroCol == this.board.length -1) {
+            neighbor = new Board(board);
+            swap(neighbor, zeroRow, zeroCol, zeroRow, zeroCol -1);
+            q.enqueue(neighbor);
+        } else {
+            neighbor = new Board(board);
+            swap(neighbor, zeroRow, zeroCol, zeroRow, zeroCol +1);
+            q.enqueue(neighbor);
+            neighbor = new Board(board);
+            swap(neighbor, zeroRow, zeroCol, zeroRow, zeroCol -1);
+            q.enqueue(neighbor);
+        }
+        return q;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
+        Board twin = new Board(this.board);
+        if (this.board[0][0] != 0 && this.board[0][1] != 0) {
+            this.swap(twin,
+                      0,
+                      0,
+                      0,
+                      1);
+        } else {
+            this.swap(twin,
+                      1,
+                      0,
+                      1,
+                      1);
+        }
+        return twin;
+    }
 
+
+    private void swap(Board initialBoard, int initialRow, int initialCol, int finalRow, int finalCol) { // exchange two elements in the array
+        int temp = initialBoard.board[initialRow][initialCol];
+        initialBoard.board[finalRow][finalCol] = initialBoard.board[initialRow][initialCol];
+        initialBoard.board[initialRow][initialCol] = temp;
     }
 
     // unit testing (not graded)
@@ -120,4 +210,3 @@ public class Board {
 
 
 }
-0
--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -4,30 +4,120 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Solver {
+    private final MinPQ<SearchNode> priorityQueue = new MinPQ<>(1);
+    private final MinPQ<SearchNode> twinPQ = new MinPQ<>(1);
 
-    // find a solution to the initial board (using the A* algorithm)
+    private class SearchNode implements Comparable<SearchNode> {
+        private Board board;
+        private SearchNode prev;
+        private int numMoves;
+
+        public SearchNode(Board initial, SearchNode previousNode, int moves) {
+            numMoves = moves;
+            board = initial;
+            prev = previousNode;
+        }
+
+
+        public int compareTo(SearchNode searchNode) {
+            return this.board.manhattan() - searchNode.board.manhattan();
+        }
+    }
+    private SearchNode goalNode;
     public Solver(Board initial) {
-
+        SearchNode start = new SearchNode(initial, null, 0);
+        SearchNode twinStart = new SearchNode(initial.twin(), null, 0);
+        // MinPQ<SearchNode> priorityQueue = new MinPQ<>(1);
+        // MinPQ<SearchNode> twinPQ = new MinPQ<>(1);
+        this.twinPQ.insert(twinStart);
+        System.out.println(twinPQ.size());
+        this.priorityQueue.insert(start);
+        SearchNode minNode = new SearchNode(null, null, 0);
+        SearchNode twinMinNode = new SearchNode(null, null, 0);
+        while (true) {
+            System.out.println("Removed a node");
+            minNode = this.priorityQueue.delMin();
+            twinMinNode = this.twinPQ.delMin();
+            if (minNode.board.manhattan() == 0) {
+                this.goalNode = minNode;
+                break;
+            }
+            if (twinMinNode.board.manhattan() == 0) {
+                this.goalNode = null;
+                break;
+            }
+            for (Board neighbor: minNode.board.neighbors()) {
+                priorityQueue.insert(new SearchNode(neighbor, minNode, (minNode.numMoves) + 1));
+            }
+            for (Board twinNeighbor: twinMinNode.board.neighbors()) {
+                priorityQueue.insert(new SearchNode(twinNeighbor, twinMinNode, 0));
+            }
+        }
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-
+        if (this.goalNode == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-
+        if (goalNode == null) {
+            return -1;
+        } else {
+            return goalNode.numMoves;
+        }
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-
+        Queue<Board> solutionOrder;
+        if (this.goalNode == null) {
+            return null;
+        }
+        else {
+            solutionOrder = new Queue<>();
+            SearchNode node = this.goalNode;
+            solutionOrder.enqueue(node.board);
+            while (node.prev != null) {
+                node = node.prev;
+                solutionOrder.enqueue(node.board);
+            }
+        }
+        return solutionOrder;
     }
 
     // test client (see below)
     public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
 
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
     }
 }
